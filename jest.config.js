ca6c6ef10f25afc1a77ceb6b8ff8b94ad99f553d
hao.chen8 <hc222ig@student.lnu.se>
2024-12-13 14:23:00 +0100

    RED: Add find-config-test

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
+// Jest configuration
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
 });
 
-/ @type {import('jest').Config} */
+/** @type {import('jest').Config} */
 const customJestConfig = {
-  testMatch: ['/*.test.(js|jsx|ts|tsx)'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/*.test.{js,jsx,ts,tsx}'],
   testEnvironment: 'jsdom',
 };
 
