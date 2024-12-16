8fe0cd55766083d0bcfb8404a4587ea87b7bd97b
Kelvin Wallace <kw222sw@student.lnu.se>
2024-12-16 15:10:48 +0100

    GREEN: Added mock object and switched test env to node instead of the MessageChannel API.

--- test/link-rendering.test.ts
+++ test/link-rendering.test.ts
@@ -3,6 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/server'
 import Link from 'next/link'
 
+
+/**
+ * @jest-environment node
+ */
+jest.mock('react-dom/server', () => ({
+    renderToString: jest.fn(() => '<a href="/my-path">to another page</a>'),
+  }));  
+
 describe('Link rendering', () => {
   it('should render Link on its own', async () => {
     const element = React.createElement(
