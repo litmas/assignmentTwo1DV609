13cf7b68cab314264cf444bf847c5a2b8062b6cb
Kelvin Wallace <kw222sw@student.lnu.se>
2024-12-16 15:21:02 +0100

    RED: getting error: Test suite failed to run, same as link-rendering.test.ts. Add a mock object to fix this.

--- test/link-rendering.test.ts
+++ test/link-rendering.test.ts
@@ -8,7 +8,7 @@ import Link from 'next/link'
  * @jest-environment node
  */
 jest.mock('react-dom/server', () => ({
-    renderToString: jest.fn(() => '<a href="/my-path">to another page</a>'),
+    renderToString: jest.fn(() => '<a href="/my-path">to another page</a>'),                 // is there anything to refector here???
   }));  
 
 describe('Link rendering', () => {
