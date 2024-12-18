5f3a8f63c2c1f13a70a2203c8b701adb1b068466
Kelvin Wallace <kw222sw@student.lnu.se>
2024-12-18 14:03:43 +0100

    RED: Eslint-v8 is not working.

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,14 @@ import { TextDecoder, TextEncoder } from 'util';
 
 global.TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
+
+module.exports = {
+  setupFilesAfterEnv: ['jest-extended'],
+};
+
+module.exports = {
+    moduleNameMapper: {
+      '^eslint-v8$': '<rootDir>/node_modules/eslint',
+    },
+  };
+  
